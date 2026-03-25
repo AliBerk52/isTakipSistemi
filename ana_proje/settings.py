055a9612e087d1d3826cdb0c2aaa872ba59689df
hazal-hk <hazal.karayigit.06@gmail.com>
2026-03-26 00:06:29 +0300

    BAC engeli için sarmal fonksiyon yazıldı, bruteforce için paket eklendi, oturum zaman aşımı eklendi

--- ana_proje/settings.py
+++ ana_proje/settings.py
@@ -115,3 +115,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+
+SESSION_COOKIE_AGE = 1800  #oturumun zaman aşımı 30 dakika
+SESSION_EXPIRE_AT_BROWSER_CLOSE = True #tarayıcı kapanırsa çıkış
+SESSION_COOKIE_SECURE = True #sadece https üzerinden cookie gönderr
