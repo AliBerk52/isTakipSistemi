6dfc367002b7a74765605344a8847cd65f65b120
Ali Berk Bacaksız <bacaksizaliberk021@gmail.com>
2026-05-10 04:27:18 +0300

    db clouda aktarıldı

--- ana_proje/settings.py
+++ ana_proje/settings.py
@@ -9,12 +9,20 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
+import os
+import dj_database_url
 from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.environ.get('mysql://root:qfvMLttpvQhFpkFsfBQlXTHTlrUJlqzh@turntable.proxy.rlwy.net:24805/railway'),
+        conn_max_age=600
+    )
+}
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
@@ -25,7 +33,11 @@ SECRET_KEY = 'django-insecure-!mf_ssqa4^whr&5819@4-)*_$35un!(&f9q^@ebeyko6w)z!0f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '.vercel.app',     
+    '127.0.0.1',
+    'localhost',
+]
 
 
 # Application definition
