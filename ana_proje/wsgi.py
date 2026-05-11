44fb8c95b7172386a1f98949ff5f4d709f595968
Ali Berk Bacaksız <bacaksizaliberk021@gmail.com>
2026-05-11 15:03:49 +0300

    wsgi.py ye django setup() fonskiyonu eklendi ve import edildi

--- ana_proje/wsgi.py
+++ ana_proje/wsgi.py
@@ -9,9 +9,12 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 
 import os
 
+import django
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ana_proje.settings')
+
+django.setup()  # Bu satır, Django'nun ayarlarını yükler ve uygulamanın düzgün çalışmasını sağlar
 
 application = get_wsgi_application()
 
